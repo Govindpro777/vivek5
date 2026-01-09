00fd330169dcff6df769e42e8892274623950db8
govindweb777 <prajapatigovind866@gmail.com>
2026-01-09 21:34:45 +0530

    first commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -109,7 +109,7 @@ const Index = () => {
         {/* <WhyLifeMatters /> */}
         {/* <WhitePaper /> */}
         <HowToBuy />
-        <PreSale />
+        {/* <PreSale /> */}
         <Tokenomics />
         <TokenDistribution />
         <Roadmap />
